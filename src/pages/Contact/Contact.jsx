eebae5b684c990d8218a6e48c983c0a377b99887
Romain Tello <rtello@iteatime.fr>
2019-09-12 11:30:49 +0200

    added working form

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,10 +1,92 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import { Layout } from '../../components';
+
+const InputBlock = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  margin-bottom: 1em;
+`;
+
+const ErrorMessage = styled.div`
+  font-size: .75em;
+  color: darkred;
+`;
+
+class TextInput extends Component {
+  state = {
+    value: '',
+    errorMessage: '',
+  }
+
+  handleChange = (event) => {
+    const { required, validate } = this.props;
+
+    const value = event.target.value;
+
+    let errorMessage = '';
+
+    if (validate) {
+      if (!validate(value)) {
+        errorMessage = 'Le contenu du champ n\'est pas valide';
+      }
+    }
+
+    if (required && value.length === 0) {
+      errorMessage = 'Le champ ne doit pas être vide';
+    }
+
+    this.setState({
+      value,
+      errorMessage,
+    });
+  }
+
+  render() {
+    const { name } = this.props;
+    const { value, errorMessage } = this.state;
+
+    return (
+      <InputBlock>
+        <label htmlFor={name}>{name}</label>
+        <input
+          name={name}
+          type="text"
+          value={value}
+          onChange={this.handleChange} />
+        <ErrorMessage>{errorMessage}</ErrorMessage>
+      </InputBlock>
+    );
+  }
+}
+
+TextInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  validate: PropTypes.func,
+};
+
+const validateEmail = (value) => {
+  return value.match(/^[\w\.\-]+@[\w\.\-]+\.\w+$/);
+}
+
+class ContactForm extends Component {
+  render() {
+    return (
+      <form>
+        <TextInput name="lastName" required />
+        <TextInput name="firstName" />
+        <TextInput name="email" validate={validateEmail} required />
+      </form>
+    );
+  }
+} 
 
 const Contact = () =>
   <Layout>
     <h1>Contactez-moi!</h1>
+    <ContactForm />
   </Layout>
 ;
 
